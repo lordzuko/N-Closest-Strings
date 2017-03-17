b296d90361132d62894ed9fb5cd6d75b4e134d6a
Himanshu Maurya <himanshumaurya2214225@hotmail.com>
2017-03-17 15:25:32 +0530

    Formatted print statement

--- NClosestStrings/topNString.py
+++ NClosestStrings/topNString.py
@@ -27,9 +27,10 @@ def main(word_list, N):
     max_cost = 4
     for x in word_list:
         closest_strings = trie_lev_distance.search(x, max_cost)
-        closest_strings.sort(key=itemgetter(1, 0))
+        closest_strings.sort(key=itemgetter(1))
         closest_strings = [s[0] for s in closest_strings]
-        print("The closest {} items for {} is {}".format(N,x,','.join(closest_strings[1:N+1])))
+
+        print("The closest {} items for {} is {}".format(N,x,','.join(sorted(closest_strings[1:N+1]))))
 
 
 if __name__ == "__main__":
@@ -38,5 +39,6 @@ if __name__ == "__main__":
     # 2 -> N
     #TODO: check the number of arguments later
     #main(sys.argv[1:])
-    word_list = ['1','2','3','4','5','6']
+    #word_list = ['1','2','3','4','5','6']
+    word_list = ['cat','dog','god','cats','cactus']
     main(word_list,3)
